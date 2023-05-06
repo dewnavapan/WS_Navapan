9079541a75f728f3f582ff1927ec774d67be3b69
dewnavapan <125631063+dewnavapan@users.noreply.github.com>
2023-05-06 10:16:33 +0700

    Initial commit

---
+++ src/components/contact.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-container>
+      <v-row class="text-center">
+        <v-col class="mb-4">
+          <h1 class="text-h3 font-weight-bold mb-3">CONTACT</h1>
+
+          <p class="subheading font-weight-regular">
+            <br />
+            Email : dewmavapan1@gmail.com <br />FB : Dew Navapan <br />
+            IG : dewnavapan <br />
+            Phone : 0611730232
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "contact",
+
+  data: () => ({}),
+};
+</script>
